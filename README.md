0023987b42dac199d25194572229468b8f48b9a8
Oliver Charles <oliver.g.charles@googlemail.com>
2012-06-19 16:17:57 +0100

    Import WIP

--- README.md
+++ README.md
@@ -1,4 +1,66 @@
-varnish-cache-invalidation
-==========================
+# Varnish Cache Invalidation Via PostgreSQL and RabbitMQ
 
-This project contains my prototype for doing Varnish cache invalidation with RabbitMQ and pg_amqp
+This project contains my prototype for doing Varnish cache invalidation with
+RabbitMQ and pg_amqp.
+
+## Installation
+
+You will need:
+
+* A PostgreSQL database, >= 9.2
+* RabbitMQ
+* [pg_amqp](https://github.com/omniti-labs/pg_amqp)
+* Haskell dependencies:
+  * resourcet, aeson, bytestring, text, postgresql-simple, amqp, http-conduit, hslogger
+* Varnish
+
+## Usage
+
+1. Install pg_amqp from source
+2. Run `CREATE EXTENSION amqp` in your database
+3. Add a broker with: `INSERT INTO amqp.broker (host, username, password) VALUES ('localhost', 'guest', 'guest')'. Change parameters to suit your installation of RabbitMQ.
+4. Install RabbitMQ event triggers. As a user with access to the `amqp` schema (ie, `postgres'), run the following:
+    ```
+    CREATE OR REPLACE FUNCTION rabbitmq()
+    RETURNS TRIGGER AS $$
+      BEGIN
+        IF TG_OP = 'INSERT' THEN
+          PERFORM amqp.publish(1, 'pg', 'database', row_to_json(e)::text)
+            FROM (VALUES ('artist', lower(TG_OP), NEW))
+              AS e ("table", event, "new");
+        ELSIF TG_OP = 'DELETE' THEN
+          PERFORM amqp.publish(1, 'pg', 'database', row_to_json(e)::text)
+            FROM (VALUES ('artist', lower(TG_OP), OLD))
+              AS e ("table", event, "old");
+        ELSIF TG_OP = 'UPDATE' THEN
+          PERFORM amqp.publish(1, 'pg', 'database', row_to_json(e)::text)
+            FROM (VALUES ('artist', lower(TG_OP), OLD, NEW))
+              AS e ("table", event, "old", "new");
+        END IF;
+        RETURN NULL;
+      END;
+    $$ LANGUAGE 'plpgsql' VOLATILE
+    SECURITY DEFINER;
+
+    CREATE OR REPLACE FUNCTION install_rabbitmq(table_name regclass)
+    RETURNS void AS $$
+      BEGIN
+        EXECUTE
+          'CREATE TRIGGER rabbitmq AFTER INSERT OR DELETE OR UPDATE ' ||
+          'ON ' || table_name || ' FOR EACH ROW EXECUTE PROCEDURE rabbitmq()';
+      END;
+    $$ LANGUAGE 'plpgsql' VOLATILE;
+
+    CREATE OR REPLACE FUNCTION uninstall_rabbitmq(table_name regclass)
+    RETURNS void AS $$
+      BEGIN
+        EXECUTE
+          'DROP TRIGGER rabbitmq ON ' || table_name;
+      END;
+    $$ LANGUAGE 'plpgsql' VOLATILE;
+    ```
+5. Install some triggers with `SELECT install_rabbitmq('artist')`. Paramater is any table name.
+6. Run the daemon with `runghc amqp.hs`
+7. Setup Varnish to use the varnish.acl file in this repository.
+8. Run `musicbrainz-server` on port 5000, and Varnish on port 9000.
+9. Change some rows in your PostgreSQL database and notice what happens
---
+++ amqp.hs
@@ -0,0 +1,126 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+import           Control.Applicative ((<*>), (<$>))
+import           Control.Monad (forever)
+import           Control.Monad.IO.Class (liftIO)
+import           Control.Monad.Reader (ReaderT, asks, runReaderT)
+import           Control.Monad.Trans.Resource (ResourceT, runResourceT)
+import           Data.Aeson (FromJSON(..), decode, (.:), Object, Value(..))
+import           Data.Aeson.Types (Parser)
+import qualified Data.ByteString.Lazy.Char8 as LBS
+import           Data.Text (Text)
+import           Database.PostgreSQL.Simple (Connection, connect, defaultConnectInfo, Only(..), query, ConnectInfo(..))
+import           Network.AMQP (consumeMsgs, Ack(..), queueName, bindQueue, openChannel, declareQueue, newQueue, ackEnv, openConnection, msgBody, Message, Envelope, declareExchange, newExchange, exchangeName, exchangeType)
+import           Network.HTTP.Conduit (httpLbs, withManager, parseUrl, method, Manager, newManager, def)
+import           System.Log.Logger (debugM, infoM, errorM, updateGlobalLogger, setLevel, Priority(..))
+
+-- | A basic environment the script runs in.
+data Env = Env { pgConn :: Connection, httpManager :: Manager }
+
+type Script a = ReaderT Env (ResourceT IO) a
+
+-- | There are 3 different classes of events - updates, which contain an old and
+-- new 'Row'; inserts, which contain a single new row; and deletes, which
+-- contain just the deleted row.
+data Event = Update Row Row | Insert Row | Delete Row
+  deriving (Show)
+
+-- | The various different rows inside the database, with the columns that are
+-- important for cache invalidation.
+data Row = Artist { artistId :: Int, artistMbid :: String }
+  deriving (Show)
+
+-- | Map a table name to a JSON parser. May fail, if we don't have a parser for
+-- this table (which means we probably aren't interested in its contents for
+-- cache invalidation.)
+jsonRowMapping :: String -> Maybe (Object -> Parser Row)
+jsonRowMapping tableName = case tableName of
+  "artist" -> return $ \o -> Artist <$> o .: "id" <*> o .: "gid"
+  _ -> Nothing
+,
+instance FromJSON Event where
+  parseJSON (Object v) = do
+      tableName <- v .: "table"
+      event     <- v .: "event"
+      case jsonRowMapping tableName of
+        Nothing -> fail $ "Unknown table " ++ tableName
+        Just m -> do
+          case event of
+            "insert" -> Insert <$> takeData v "new" m
+            "delete" -> Delete <$> takeData v "old" m
+            "update" -> Update <$> takeData v "old" m <*> takeData v "new" m
+            _ -> fail (event ++ " is not a recognised event type")
+      where takeData v l m = (v .: l) >>= m
+
+  parseJSON _ = fail "Expected a JSON object"
+
+rowChanged :: Row -> Script ()
+
+rowChanged (Artist id mbid) = do
+    -- Clear the entire artist
+    addBan $ "/artist/" ++ mbid
+
+    --
+    liftIO $ infoM "MBCacheInvalidator" ("Finding artist credits that refer to " ++ mbid)
+    invalidateLinkedArtistCredits
+  where invalidateLinkedArtistCredits = do
+          conn <- asks pgConn
+          liftIO (query conn "SELECT artist_credit FROM musicbrainz.artist_credit_name WHERE artist = ?" (Only id)) >>= mapM_ (invalidateArtistCredit . fromOnly)
+
+invalidateArtistCredit :: Int -> Script ()
+invalidateArtistCredit id = do
+  c <- asks pgConn
+  (liftIO $ query c "SELECT gid::text FROM musicbrainz.recording WHERE artist_credit = ?" (Only id))
+    >>= mapM_ (addBan . ("/recording/" ++) . fromOnly)
+
+varnishPrefix :: String
+varnishPrefix = "http://localhost:9000/ws/2"
+
+addBan :: String -> Script ()
+addBan path = do
+  manager <- asks httpManager
+  initReq <- parseUrl $ varnishPrefix ++ path
+  let req = initReq { method = "BAN" }
+  liftIO $ infoM "MBCacheInvalidator" $ "Adding ban " ++ path
+  res <- httpLbs req manager
+  liftIO $ debugM "MBCacheInvalidator" $ "Ban response: " ++ show res
+
+handleEvent :: Event -> Script ()
+handleEvent e = mapM_ rowChanged rows
+  where rows = case e of
+                 Update old new -> [ old, new ]
+                 Insert new -> return new
+                 Delete old -> return old
+
+receiveMessage :: (Message, Envelope) -> Script ()
+receiveMessage (msg, env) = do
+  case decode (msgBody msg) of
+    Nothing -> liftIO $ do
+      errorM "MBCacheInvalidator" $ "Error occured handling event"
+      errorM "MBCacheInvalidator" $ "Message body: " ++ (LBS.unpack $ msgBody msg)
+    Just event -> do
+      liftIO $ debugM "MBCacheInvalidator" $ "Handling: " ++ show event
+      handleEvent event
+  liftIO $ ackEnv env
+
+main :: IO ()
+main = do
+    updateGlobalLogger "MBCacheInvalidator" (setLevel INFO)
+
+    conn <- openConnection "127.0.0.1" "/" "guest" "guest"
+    chan <- openChannel conn
+
+    -- declare a queue, exchange and binding
+    declareQueue chan newQueue {queueName = "database"}
+    declareExchange chan newExchange {exchangeName = "pg", exchangeType = "fanout"}
+    bindQueue chan "database" "pg" "database"
+
+    -- subscribe to the queue
+    buildCallback >>= consumeMsgs chan "database" Ack
+
+    forever getLine
+  where buildCallback = do pgConn <- connect defaultConnectInfo { connectDatabase = "musicbrainz" }
+                           httpManager <- newManager def
+                           let env = Env { pgConn = pgConn, httpManager = httpManager }
+                           return $ \cb -> runResourceT $ runReaderT (receiveMessage cb) env
